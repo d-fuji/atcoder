a52bf1dadca1cfcceb2795936a3986f6a9d1b336
daiki-fujii <d0a5i16ki@gmail.com>
2021-07-03 20:43:22 +0900

    add null check

--- AtcoderBiginnerContest/TokyoMarinNichido/main.dart
+++ AtcoderBiginnerContest/TokyoMarinNichido/main.dart
@@ -5,21 +5,21 @@ void main() {
 }
 
 String Nickname() {
-  String string = stdin.readLineSync();
+  String string = stdin.readLineSync()!;
   String s = string.substring(0, 3);
   return s;
 }
 
 String Tag() {
-  String str_av = stdin.readLineSync();
+  String str_av = stdin.readLineSync()!;
   List list_av = str_av.split(' ');
-  String str_bw = stdin.readLineSync();
+  String str_bw = stdin.readLineSync()!;
   List list_bw = str_bw.split(' ');
   int a = int.parse(list_av[0]);
   int v = int.parse(list_av[1]);
   int b = int.parse(list_bw[0]);
   int w = int.parse(list_bw[1]);
-  int t = int.parse(stdin.readLineSync());
+  int t = int.parse(stdin.readLineSync()!);
 
   if ((a - b).abs() <= t * (v - w)) {
     return 'YES';
